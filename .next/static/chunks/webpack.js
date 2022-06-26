fbab8d17ad7c6a24f46052f0b4d61c6351ba5bcb
Orhero Onome <orheroonome@gmail.com>
2022-06-26 16:46:35 +0100

    another commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a4a02091669501db"; }
+/******/ 		__webpack_require__.h = function() { return "fb0aad056c0e15e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
-import Butter from "buttercms";
 import Head from "next/head";
 import Script from 'next/script';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { FiShoppingBag } from 'react-icons/fi';
+
 
 const butter = require('buttercms')('69cb07d577457e0eb1ac7e197b31e4f94eb41825');
 
@@ -27,26 +29,26 @@ const cookies = ({ cookies }) => {
         <link rel="preconnect" href="https://cdn.snipcart.com" />
         <link rel="stylesheet" href="https://cdn.snipcart.com/themes/v3.2.0/default/snipcart.css" />
       </Head>
-
       <main className="container">
-        <h3 className="text-5xl m-10 title">Shop
-          <span className="float-right text-red-700 snipcart-checkout title">Cart</span>
-        </h3>
+      
+        <h1 className="text-xl md:text-3xl m-2 md:m-5 font-bold font-mono text-green-700">Phone-mall
+          <span className="float-right text-l md:text-2xl p-2 rounded-lg bg-transparent hover:bg-purple-300 text-purple-900 snipcart-checkout title">  <span className="float-left"><FiShoppingBag size={25}/></span> View Cart</span>
+        </h1>
         <hr className="style-eight" />
         <div className="grid place-items-center mt-10 gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {cookies.map(cookies => (
 
-            <div key={cookies.id} className='text-blue-700 relative mb-10 max-w-sm rounded shadow-lg hover:shadow-xl h-full'>
+            <div key={cookies.id} className=' relative mb-10 max-w-sm rounded shadow-lg hover:shadow-xl h-full'>
               <img src={cookies.image} alt={`${cookies.name}`} className="w-full" />
               <div className="px-6 py-4 ">
-                <div className="text-3xl mb-5">
-                  <h3>{cookies.name}
+                <div className="text-2xl mb-5">
+                  <h3 className="text-purple-900 font-bold uppercase">{cookies.name}
                     <span className="float-right text-green-700"> ${cookies.price}</span>
                   </h3>
                 </div>
                 <p className='flex-1'>{cookies.description}</p> <br /><br /><br />
                 <button className="absolute bottom-10 left-1/2 transform 
-                -translate-x-1/2 w-64 btn flex-1 text-2xl 
+                -translate-x-1/2 w-full btn flex-1 text-xl 
                 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold 
                 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent 
                 items-center rounded justify-self-center mt-10 snipcart-add-item"
@@ -57,7 +59,7 @@ const cookies = ({ cookies }) => {
                   data-item-description={cookies.description}
                   data-item-price={cookies.price}
                 >
-                  Add To Cart
+                   <span className="float-left"><MdAddShoppingCart size={25}/></span>Add To Cart  
 
                 </button>
               </div>
